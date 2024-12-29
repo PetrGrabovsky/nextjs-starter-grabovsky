63b4c4b8f56293abdc50830d0ac2fb74a1aff88f
Petr Grabovský <petr@grabovsky.tech>
2024-12-29 17:10:36 +0100

    chore: Přidej Prettier plugin pro Prisma
    - Nainstaluj 'prettier-plugin-prisma'
    - Aktualizuj konfiguraci Prettier pro formátování '.prisma' souborů
    - Přidej '.prisma' soubory do 'lint-staged'

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
   endOfLine: 'lf',
   htmlWhitespaceSensitivity: 'css',
   jsxSingleQuote: true,
-  plugins: ['prettier-plugin-tailwindcss'],
+  plugins: ['prettier-plugin-tailwindcss', 'prettier-plugin-prisma'],
   printWidth: 100,
   proseWrap: 'preserve',
   quoteProps: 'as-needed',

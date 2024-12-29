71e6c14733c3cc18771cb0f05fb52ff507eb9ec2
Petr Grabovský <petr@grabovsky.tech>
2024-12-29 21:17:20 +0100

    chore: Přidej ESLint plugin pro bezpečný kód
    - Nainstaluj 'eslint-plugin-security'
    - Aktualizuj konfiguraci ESLint s maximálně přísnými pravidly

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import { FlatCompat } from '@eslint/eslintrc';
 import promisePlugin from 'eslint-plugin-promise';
+import securityPlugin from 'eslint-plugin-security';
 import simpleImportSort from 'eslint-plugin-simple-import-sort';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
@@ -22,12 +23,22 @@ const eslintConfig = [
     plugins: {
       'simple-import-sort': simpleImportSort,
       promise: promisePlugin,
+      security: securityPlugin,
     },
     rules: {
       'prettier/prettier': 'error',
       'simple-import-sort/imports': 'error',
       'simple-import-sort/exports': 'error',
       'promise/always-return': 'off',
+      'security/detect-unsafe-regex': 'error',
+      'security/detect-non-literal-require': 'error',
+      'security/detect-non-literal-fs-filename': 'error',
+      'security/detect-eval-with-expression': 'error',
+      'security/detect-child-process': 'error',
+      'security/detect-object-injection': 'error',
+      'security/detect-buffer-noassert': 'error',
+      'security/detect-disable-mustache-escape': 'error',
+      'security/detect-possible-timing-attacks': 'error',
     },
   },
 ];
